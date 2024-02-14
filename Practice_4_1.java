7cc3e72ad481eef796a5257ae418a7ee9889e3ff
Matcha2Smiley <greentea2Smiley@gmail.com>
2024-02-14 22:10:40 +0900

    第4章完

--- Practice_4_1.java
+++ Practice_4_1.java
@@ -1,5 +1,8 @@
 public class Practice_4_1{
     public static void main(String[] args){
-        int[] points = int[5];
+        int[] points = int[4];
+        double[] weights = double[5];
+        boolean[] answers = boolean[3];
+        String[] names = String[3];
     }
 }
